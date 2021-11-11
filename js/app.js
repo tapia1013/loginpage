27f0644f3d1cbe59c47cd33a8916545332080cc6
tapia1013 <manbearpig1013@gmail.com>
2021-11-10 19:29:21 -0800

    recreate the whole html with pure js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
+// returns an array
+let root = document.getElementsByClassName('root')[0];
+let card = document.createElement('section');
+// add class of card to section
+card.classList.add('card');
+let style = document.createElement('style')
 
 
 
+
+root.style.cssText = `
+  background: url('https://wallpaperaccess.com/full/636909.jpg');
+  background-size: cover;
+  background-position: center;
+`;
+// s3 js part 1 @13:39
